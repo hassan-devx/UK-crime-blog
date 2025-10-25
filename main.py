b4074dcded1839ef77470f3bab30781008db9d75
Hassan Akinleye <hakinleye1@gmail.com>
2025-10-25 16:49:08 +0100

    Trigger migration on startup via upgrade()

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from app import create_app, db
 app = create_app()
 
 if __name__ == '__main__':
-   # with app.app_context():
-    #    db.create_all()
-
+   if __name__ == "__main__":
+    from flask_migrate import upgrade
+    upgrade()
+    app.run(host="0.0.0.0", port=8080)
     app.run(debug=True)
 
